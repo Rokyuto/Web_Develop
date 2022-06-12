28c1415f2998e581caffca2571ffe6d32af86306
Rokyuto <93655294+Rokyuto@users.noreply.github.com>
2022-06-12 17:35:05 +0300

    Finish Snake Game

--- SnakeGame/game.js
+++ SnakeGame/game.js
@@ -1,18 +1,30 @@
 //import {f_Update as f_snakeUpdate , f_Draw as f_snakeDraw , SNAKE_SPEED } from './snake.js'
 
-const SNAKE_SPEED = 1 // Snake moves 1 times per second
+const GRID_SIZE = 21 // Grid size
+const SNAKE_SPEED = 2 // Snake moves 2 times per second
 let lastRenderTime = 0
+const EXPANSION_RATE = 1 // How much the snake grow when eats food - 1 piece
+let newSegments = 0
+let gameOver = false
 
 let inputDirection = {x: 0, y: 0} // Initial snake position
 let lastInputDirection = {x: 0, y: 0} // Last snake position
 const snakeBody = [{ x: 10, y: 11 }] // Array with snake's segments and their XY positions in the grid
 
-let food = {x: 10, y: 1 }
+let food = f_randomFoodPosition() // Generate random food position on game start
 
 const gameBoard = document.getElementById('gameBoard') // Get the game board from the main document/page (index.html)
 
 // Loop Function
 function f_mainLoop(currentTime) {
+
+    if(gameOver) { // If game over is true then
+        // Game Over Alert
+        if(confirm("Game Over! Press OK to restart the game.")){ // If OK pressed, then restart the game
+            window.location.reload(); // Reload the page
+        }
+        return 
+    }
 
     window.requestAnimationFrame(f_mainLoop) // Request when is the Next Frame Render
 
@@ -33,6 +45,9 @@ window.requestAnimationFrame(f_mainLoop) // Call Loop Function
 
 // Update Function - Update Snake Lenght, Snake Movement
 function f_Update() {
+
+    f_addSegment() // Call Function to add segment to the snake on eaten food
+
     const inputDirect = f_getInputDirection() // Update the Snake Direction based on the Inputed Direction
     f_SnakeUpdate() // Call Function to Update the Snake Fragments' positions
 
@@ -40,7 +55,9 @@ function f_Update() {
     snakeBody[0].x += inputDirect.x // X Direction
     snakeBody[0].y += inputDirect.y // Y Direction
 
-    f_FoodUpdate()
+    f_FoodUpdate() // Call Function to Update the Food
+
+    f_checkDeath() // Call Function to check for Death
 
 }
 
@@ -56,6 +73,7 @@ function f_Draw(gameBoard) {
 
 // Function to Iterate the snakeBody and update the fragments positions
 function f_SnakeUpdate(){
+
     // Second-to-last segment of the snake body loop 
     for (var i = snakeBody.length - 2; i >= 0; i--) // From bottom to top of the snake
     {
@@ -66,7 +84,35 @@ function f_SnakeUpdate(){
 }
 
 function f_FoodUpdate() {
+    if(f_onSnake(food)) { // Call Function to check if the food is on any snake segment -> If TRUE, continue
+        f_expandSnake(EXPANSION_RATE) // Call Function to expand the snake by one
+        food = f_randomFoodPosition() // New Food Position
+    }
+}
 
+// Function to increase the snake segments by one
+function f_expandSnake(amount) {
+    newSegments += amount // Increment new segments quantity bt one (expansion rate)
+}
+
+// Function to check if the food is on the position of any snake segment
+function f_onSnake(position, {ignoreHead = false} = {}){
+    return snakeBody.some(( segment, index ) =>{ // Will iterate over all snake segments
+        if( ignoreHead && index === 0 ) return false // If ignoreHead is true and segment index is 0 then return false (snake Head is on snake Head postion) -> snake Head doesn't touch other snake segment
+        return f_equalPosition(position, segment) // Call Function to Compare Food and Segment Positions 
+    })
+}
+
+// Function to Compare the food and Segment Positions
+function f_equalPosition(pos1,pos2) {
+    return pos1.x === pos2.x && pos1.y === pos2.y // Will Return True if Food and Segment positions are equal
+}
+
+function f_addSegment(){
+    for (let i = 0; i < newSegments; i++) {
+        snakeBody.push( {...snakeBody[snakeBody.length - 1] } ) // Add new segment (duplicate of the last snake segment) at the end of the snake
+    }
+    newSegments = 0 // Reset the new segments quantity
 }
 
 window.addEventListener('keydown',e => {
@@ -90,6 +136,7 @@ window.addEventListener('keydown',e => {
     }
 })
 
+
 function f_getInputDirection(){
     lastInputDirection = inputDirection // Update the last input direction
     return inputDirection // Return the direction
@@ -107,10 +154,50 @@ function f_DrawSnake(gameBoard){
     })
 }
 
+// Draw Food Function
 function f_DrawFood(gameBoard){
     const foodElement = document.createElement("div") // Create div element (food segment) on the main document (gameBoard)
     foodElement.style.gridRowStart = food.y // Draw the Current foodfood Segment on a row with x location
     foodElement.style.gridColumnStart = food.x // Draw the Current food Segment on a column with y location
     foodElement.classList.add('food') // Add the food element to the food class list to be sure it will be visualized
     gameBoard.appendChild(foodElement) // Append the current food element to the gameBoard as child
+}
+
+// Function to Generate Random Food Location
+function f_randomFoodPosition(){
+    let newFoodPosition
+    while (newFoodPosition == null || f_onSnake(newFoodPosition)){ // While new food position is null or any snake segment is on the current food position, then
+        newFoodPosition = f_randomGridPosition() // Call Function to Generate random new food position
+    }
+    return newFoodPosition // Return the new food position
+}
+
+// Function to Generate Random Grid Postion
+function f_randomGridPosition(){
+    return {
+        x: Math.floor(Math.random() * GRID_SIZE) + 1, // X position
+        y: Math.floor(Math.random() * GRID_SIZE) + 1 // Y position
+    }
+}
+
+// Function to check for death
+function f_checkDeath(){
+    // Game Over when Snake Head is out of the grid or 
+    gameOver = f_outsideGrid(f_getSnakeHead())      ||   f_snakeIntersection()
+}
+
+// Function to check for outside of grid position
+function f_outsideGrid(position){
+    return( position.x < 1 || position.x > GRID_SIZE || 
+            position.y < 1 || position.y > GRID_SIZE )
+}
+
+// Function to get snake head
+function f_getSnakeHead(){
+    return snakeBody[0] // Get the snake head postion
+}
+
+// Function to check if the snake head is touching other snake segment
+function f_snakeIntersection() {
+    return f_onSnake(snakeBody[0] , {ignoreHead: true}); // Return true if the snake head is touching other snake segment
 }
